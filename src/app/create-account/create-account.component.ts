a1bfefd2364a2d7be365349af54bae237ad7e17e
Naresh Kumar H <nareshkumarh@live.com>
2020-10-29 13:24:16 +0530

    Create Account and verify account feature

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -8,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountComponent implements OnInit {
 
+  loggedInUser;
   constructor() { }
 
   ngOnInit(): void {
+    this.loggedInUser = JSON.parse(localStorage.LOGGED_IN_USER);
   }
 
+  accountType:string;
+
+  createAccount(){
+    console.log("AccountType" , this.accountType);
+    let formData = { accountNo: 1 , userId: this.loggedInUser.id , accountType: this.accountType, balance: 0 , status:"INACTIVE"};
+    console.log(formData);
+
+    let accounts = JSON.parse(localStorage.getItem("ACCOUNTS")) || [];
+    accounts.push(formData);
+    localStorage.setItem("ACCOUNTS", JSON.stringify(accounts));
+    alert("Account created successfully");
+  }
 }
--- src/app/verify-account/verify-account.component.ts
+++ src/app/verify-account/verify-account.component.ts
@@ -10,7 +10,31 @@ export class VerifyAccountComponent implements OnInit {
 
   constructor() { }
 
+  accounts;
+
   ngOnInit(): void {
+    this.accounts = JSON.parse(localStorage.getItem("ACCOUNTS")) || [];
   }
+
+
+  approve(account, index){
+    console.log("Going to approve", account);
+    account.status = "ACTIVE";
+    this.accounts[index] = account;
+    localStorage.setItem("ACCOUNTS", JSON.stringify(this.accounts) );
+    
+  }
+
+  
+  disableAccount(account, index){
+    console.log("Going to approve", account);
+    account.status = "INACTIVE";
+    this.accounts[index] = account;
+    localStorage.setItem("ACCOUNTS", JSON.stringify(this.accounts) );
+    
+  }
+
+
+
 
 }
